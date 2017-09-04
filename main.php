563c21a34ffb0b8efafc46a33bcfd12fa37496c5
Programster <sdpagent@gmail.com>
2017-09-04 14:03:22 +0000

    Adding a distro for now using the debian 8 os variant and kickstart file for now. Need to test this.

--- main.php
+++ main.php
@@ -39,8 +39,16 @@ $distros = array(
     ),
     new Distro(
        'Debian 8', 
-       'debian8', # jessie not yet listed in os variants
+       'debian8', 
        'http://ftp.us.debian.org/debian/dists/jessie/main/installer-amd64/',
+       'http://pastebin.com/raw.php?i=3NmQCK42',
+       "url",
+       "auto=true text hostname=debian domain=debian"
+    ),
+    new Distro(
+       'Debian 9', 
+       'debian8', # stretch not yet listed in os variants yet
+       'http://ftp.us.debian.org/debian/dists/stretch/main/installer-amd64/',
        'http://pastebin.com/raw.php?i=3NmQCK42',
        "url",
        "auto=true text hostname=debian domain=debian"
